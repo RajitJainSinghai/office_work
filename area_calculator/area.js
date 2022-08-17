6c7217b543e34046845d8c65db1239e945afbd9b
Rajit jain <noddysinghai053@gmail.com>
2022-08-17 16:05:38 +0530

    added new code

--- area_calculator/area.js
+++ area_calculator/area.js
@@ -1,12 +1,10 @@
+/********* first ************************* */
 
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+$(document).ready(function(){
+  $('#show').click(function() {
+    $('.menu').toggle("slide");
+  });
+});
     function calc() {
     var textValue1 = document.getElementById('input1').value;
     var textValue2 = document.getElementById('input2').value;
@@ -14,32 +12,27 @@ function myFunction() {
     document.getElementById('output').value = textValue1 * textValue2;
     result();
 }
+/********* second************************* */
 
 
-  function myFunction1() {
-    var x = document.getElementById("myDIV1");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
+$(document).ready(function(){
+  $('#show1').click(function() {
+    $('.menu1').toggle("slide");
+  });
+});
   function calc1() {
     var textValue3 = document.getElementById('input3').value;
     var textValue4 = document.getElementById('input4').value;
     document.getElementById('output1').value = textValue3 * textValue4;
     result();
 }
+/********* third ************************* */
 
-function myFunction2() {
-  var x = document.getElementById("myDIV2");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+$(document).ready(function(){
+  $('#show2').click(function() {
+    $('.menu2').toggle("slide");
+  });
+});
   function calc2() {
   var textValue5 = document.getElementById('input5').value;
   var textValue6 = document.getElementById('input6').value;
@@ -57,3 +50,36 @@ function result() {
   
   document.getElementById("result").value = a;
 }
+
+/******************** modal ************************ */
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+/*********************************************** */
+$(document).ready(function(){
+  $('#show').click(function() {
+    $('.menu').toggle("slide");
+  });
+});
