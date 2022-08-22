b681de837fe19ce0dfc57386fe96552fd2a36f34
Rajit jain <noddysinghai053@gmail.com>
2022-08-22 12:13:06 +0530

    added new code

--- videoPage/video1.js
+++ videoPage/video1.js
@@ -1,25 +1,33 @@
-    // Get the modal
-    var modal = document.getElementById("myModal");
+$(function() {
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-    
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-    
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-    
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-    
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
+  // when the modal is shown
+  $('#myModal').on('show.bs.modal', function(e) {
+    var $modal = $(this);
+
+    // find the trigger button
+    var $button = $(e.relatedTarget);
+
+    // find the hidden div next to trigger button
+    var $notifications = $button.siblings('div.hidden');
+
+    // transfer content to modal body
+    $modal.find('.modal-body').html($notifications.html());
+  })
+});
+
+
+/******************************************* */
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var btn1 = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+btn1.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
